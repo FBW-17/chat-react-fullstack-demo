23609208f727ac564e55ebc0b2960c97a570ee5e
losrobbos <losrobbos@web.de>
2020-04-20 00:12:40 +0200

    Updates

---
+++ precache-manifest.7309cdd85ca62d2f0b587352e7b9916b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce1c5c59cd98386b2c894260d4c1f99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f449f8f1f0b2c16187a",
+    "url": "./static/css/main.a4273caf.chunk.css"
+  },
+  {
+    "revision": "537742abad648a5f0ca2",
+    "url": "./static/js/2.183e84b3.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "./static/js/2.183e84b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f449f8f1f0b2c16187a",
+    "url": "./static/js/main.14354a96.chunk.js"
+  },
+  {
+    "revision": "c999e1aafeacb3931724",
+    "url": "./static/js/runtime-main.9edb4eab.js"
+  }
+]);
